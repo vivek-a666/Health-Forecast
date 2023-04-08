889b0a120022bab2574c9ab818cd113b9c0af678
Vivek <vivek.mails09@gmail.com>
2023-04-09 00:13:59 +0530

    Health Forecast

---
+++ app.py
@@ -0,0 +1,142 @@
+import torch
+import torch.nn.functional as F
+import streamlit as st
+import pandas as pd
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+import re
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import numpy as np
+import pickle
+import torch.nn as nn
+import speech_recognition as sr
+
+
+le = pickle.load(open(r"C:\Users\pavan\Diagnosis_Prodigy\encoder (1).pkl", 'rb'))
+tokenizer = pickle.load(open(r"C:\Users\pavan\Diagnosis_Prodigy\tokenizer (1).pkl", 'rb'))
+
+def clean_text(x):
+    pattern = r'[^a-zA-z0-9\s]'
+    text = re.sub(pattern, '', x)
+    return x
+
+def clean_numbers(x):
+    if bool(re.search(r'\d', x)):
+        x = re.sub('[0-9]{5,}', '#####', x)
+        x = re.sub('[0-9]{4}', '####', x)
+        x = re.sub('[0-9]{3}', '###', x)
+        x = re.sub('[0-9]{2}', '##', x)
+    return x
+
+contraction_dict = {"ain't": "is not", "aren't": "are not","can't": "cannot", "'cause": "because", "could've": "could have", "couldn't": "could not", "didn't": "did not",  "doesn't": "does not", "don't": "do not", "hadn't": "had not", "hasn't": "has not", "haven't": "have not", "he'd": "he would","he'll": "he will", "he's": "he is", "how'd": "how did", "how'd'y": "how do you", "how'll": "how will", "how's": "how is",  "I'd": "I would", "I'd've": "I would have", "I'll": "I will", "I'll've": "I will have","I'm": "I am", "I've": "I have", "i'd": "i would", "i'd've": "i would have", "i'll": "i will",  "i'll've": "i will have","i'm": "i am", "i've": "i have", "isn't": "is not", "it'd": "it would", "it'd've": "it would have", "it'll": "it will", "it'll've": "it will have","it's": "it is", "let's": "let us", "ma'am": "madam", "mayn't": "may not", "might've": "might have","mightn't": "might not","mightn't've": "might not have", "must've": "must have", "mustn't": "must not", "mustn't've": "must not have", "needn't": "need not", "needn't've": "need not have","o'clock": "of the clock", "oughtn't": "ought not", "oughtn't've": "ought not have", "shan't": "shall not", "sha'n't": "shall not", "shan't've": "shall not have", "she'd": "she would", "she'd've": "she would have", "she'll": "she will", "she'll've": "she will have", "she's": "she is", "should've": "should have", "shouldn't": "should not", "shouldn't've": "should not have", "so've": "so have","so's": "so as", "this's": "this is","that'd": "that would", "that'd've": "that would have", "that's": "that is", "there'd": "there would", "there'd've": "there would have", "there's": "there is", "here's": "here is","they'd": "they would", "they'd've": "they would have", "they'll": "they will", "they'll've": "they will have", "they're": "they are", "they've": "they have", "to've": "to have", "wasn't": "was not", "we'd": "we would", "we'd've": "we would have", "we'll": "we will", "we'll've": "we will have", "we're": "we are", "we've": "we have", "weren't": "were not", "what'll": "what will", "what'll've": "what will have", "what're": "what are",  "what's": "what is", "what've": "what have", "when's": "when is", "when've": "when have", "where'd": "where did", "where's": "where is", "where've": "where have", "who'll": "who will", "who'll've": "who will have", "who's": "who is", "who've": "who have", "why's": "why is", "why've": "why have", "will've": "will have", "won't": "will not", "won't've": "will not have", "would've": "would have", "wouldn't": "would not", "wouldn't've": "would not have", "y'all": "you all", "y'all'd": "you all would","y'all'd've": "you all would have","y'all're": "you all are","y'all've": "you all have","you'd": "you would", "you'd've": "you would have", "you'll": "you will", "you'll've": "you will have", "you're": "you are", "you've": "you have"}
+def _get_contractions(contraction_dict):
+    contraction_re = re.compile('(%s)' % '|'.join(contraction_dict.keys()))
+    return contraction_dict, contraction_re
+contractions, contractions_re = _get_contractions(contraction_dict)
+def replace_contractions(text):
+    def replace(match):
+        return contractions[match.group(0)]
+    return contractions_re.sub(replace, text)
+
+def condition_parser(x):
+    if x in target_conditions:
+        return x
+    else:
+        return "OTHER"
+
+maxlen = 750
+embed_size = 300
+max_features = 120000
+
+class CNN_Text(nn.Module):
+    
+    def __init__(self):
+        super(CNN_Text, self).__init__()
+        filter_sizes = [1,2,3,5]
+        num_filters = 36
+        n_classes = len(le.classes_)
+        self.embedding = nn.Embedding(max_features, embed_size)
+        self.embedding.weight = nn.Parameter(torch.tensor(embedding_matrix, dtype=torch.float32))
+        self.embedding.weight.requires_grad = False
+        self.convs1 = nn.ModuleList([nn.Conv2d(1, num_filters, (K, embed_size)) for K in filter_sizes])
+        self.dropout = nn.Dropout(0.1)
+        self.fc1 = nn.Linear(len(filter_sizes)*num_filters, n_classes)
+
+
+    def forward(self, x):
+        x = self.embedding(x)  
+        x = x.unsqueeze(1)  
+        x = [F.relu(conv(x)).squeeze(3) for conv in self.convs1] 
+        x = [F.max_pool1d(i, i.size(2)).squeeze(2) for i in x]  
+        x = torch.cat(x, 1)
+        x = self.dropout(x)  
+        logit = self.fc1(x) 
+        return logit
+
+model = torch.load(r"C:\Users\pavan\Diagnosis_Prodigy\textcnn_model (1)", map_location=torch.device('cpu'))
+        
+def predict_single(x):    
+    # lower the text
+    x = x.lower()
+    # Clean the text
+    x =  clean_text(x)
+    # Clean numbers
+    x =  clean_numbers(x)
+    # Clean Contractions
+    x = replace_contractions(x)
+    # tokenize
+    x = tokenizer.texts_to_sequences([x])
+    # pad
+    x = pad_sequences(x, maxlen=maxlen)
+    # create dataset
+    x = torch.tensor(x, dtype=torch.long).cpu()
+
+    pred = model(x).detach()
+    pred = F.softmax(pred).cpu().numpy()
+
+    pred = pred.argmax(axis=1)
+
+    pred = le.classes_[pred]
+    return pred[0]
+
+def handle_voice_input():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        st.write("Say something...")
+        audio = r.listen(source)
+    try:
+        text = r.recognize_google(audio)
+        st.write("You said: ", text)
+        return text
+    except sr.UnknownValueError:
+        st.write("Could not understand audio")
+    except sr.RequestError as e:
+        st.write("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+# Define the Streamlit app
+def app():
+    st.title("Text Classification App")
+
+    # Add a button to start recording the voice
+    col1, col2 =  st.columns([1, 3])
+    with col1:
+        record_button = st.button("Record Audio")
+    with col2:
+        input_text = st.text_input("Enter text")
+
+    if record_button:
+        # Handle voice input
+        voice_text = handle_voice_input()
+        if voice_text:
+            input_text = voice_text
+
+    if input_text:
+        # Make the prediction
+        pred = predict_single(input_text)
+        st.write("Prediction:", pred)
+
+# Run the app
+if __name__ == "__main__":
+    app()
